c1fd055d94f4a62b5405d8c4f496291ba150a07f
zukonake <zeukmc@gmail.com>
2015-10-18 02:50:40 +0200

    Finished componentHandler files.

--- source/componentHandler.cpp
+++ source/componentHandler.cpp
@@ -3,21 +3,22 @@
 #include "componentHandler.hpp"
 
 template < typename componentType >
-cComponent< componentType >& returnComponent( std::string componentName )
+int16_t returnComponentIndex( std::string componentName )
 {
 	for( uint16_t i = 0; i < componentList.size(); i++ )
 	{
 		if( static_cast< cComponent< componentType >* >( componentList[i] )->name == componentName )
 		{
-			return componentList[i];
+			return i;
 		}
 	}
-	return NULL;
+	return -1;
 }
 
+template < typename componentType >
 bool isComponentPresent( std::string componentName )
 {
-	if( returnComponent( componentName ) == NULL )
+	if( returnComponentIndex< componentType >( componentName ) == -1 )
 	{
 		return false;
 	}
@@ -27,7 +28,7 @@ bool isComponentPresent( std::string componentName )
 template < typename componentType >
 componentType* returnComponentValue( std::string componentName )
 {
-	if( not isComponentPresent )
+	if( not isComponentPresent< componentType >( componentName ) )
 	{
 		return NULL;
 	}
@@ -38,11 +39,31 @@ componentType* returnComponentValue( std::string componentName )
 template < typename componentType >
 bool setComponentValue( std::string componentName, componentType newValue )
 {
-	if( not isComponentPresent( componentName ) )
+	if( not isComponentPresent< componentType >( componentName ) )
 	{
 		return false
 	}
-	cComponent< componentType >& component = returnComponent( componentName )
-	
+	static_cast< cComponent< componentType >* >( componentList[ returnComponentIndex< componentType >( componentName ) ] )->value = newValue;
+	return true;
+}
+
+bool deleteComponent( std:;string componentName )
+{
+	if( not is componentPresent< componentType >( componentName ) )
+	{
+		return false
+	}
+	componentList.erase( returnComponentIndex< componentType >( componentName ) )
+	return true;
+}
+
+template < typename componentType >
+bool insertComponent( cComponent* component )
+{
+	if( isComponentPresent< componentType >( componentName ) )
+	{
+		return false;
+	}
+	componentList.push_back( component );
 	return true;
 }
--- source/componentHandler.hpp
+++ source/componentHandler.hpp
@@ -12,14 +12,18 @@ class cComponentHandler
 {
 	std::vector < std::unique_ptr< cComponentInterface* > componentList;
 	template < typename componentType >
-	cComponent< componentType > returnComponent( std::string componentName );
+	uint16_t returnComponentIndex( std::string componentName );
 	public:
+	template < typename componentType >
 	bool isComponentPresent( std::string componentName );
 	template < typename componentType >
 	componentType returnComponentValue( std::string componentName );
 	template < typename componentType >
 	bool setComponentValue( std::string componentName, componentType newValue );
+	template < typename componentType >
 	bool deleteComponent( std::string componentName );
+	template < typename componentType >
+	bool insertComponent( cComponent* component );
 	cComponentHandler();
 }
 
