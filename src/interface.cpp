2f37d7acd2d03cace8211428b547342efe062176
zukonake <zeukmc@gmail.com>
2015-10-21 23:19:38 +0200

    Improved interface.

--- src/interface.cpp
+++ src/interface.cpp
@@ -1,7 +1,37 @@
 //interface.cpp
 #include "interface.hpp"
 
-cInterface::cInterface()
+void cInterface::print()
 {
+	box( nCursesWindow, hBorder, vBorder );
+	//print teh stuff
+	wrefresh( nCursesWindow );
+}
 
+void cInterface::clear()
+{
+	wclear( nCursesWindow );
+}
+/* not sure if needed
+void cInterface::refresh()
+{
+	wrefresh( nCursesWindow );
+}
+*/
+cInterface::cInterface( cLuaTable _componentList, std::string _name, uint16_t _posX, uint16_t _posY, uint16_t _width, uint16_t _height, char _hBorder, char _vBorder ) :
+	componentList( _componentList ),
+	name( _name ),
+	posX( _posX ),
+	posY( _posY ),
+	width( _width ),
+	height( _height ),
+	hBorder( _hBorder ),
+	vBorder( _vBorder )
+{
+	nCursesWindow = newwin( height, width, posY, posX );
+}
+
+cInterface::~cInterface()
+{
+	delwin( nCursesWindow );
 }
--- src/interface.hpp
+++ src/interface.hpp
@@ -1,10 +1,18 @@
 //interface.hpp
 #ifndef INTERFACE_HPP
 #define INTERFACE_HPP
+#include <vector>
+#include <ncurses.h>
+#include "lua/luaTable.hpp"
 
 class cInterface
 {
-	cInterface();
+	std::string name;
+	luaTable componentList;
+	WINDOW* nCursesWindow;
+	void print();
+	// void refresh(); not sure if needed
+	cInterface( cLuaTable _componentList, std::string _name, uint16_t _posX, uint16_t _posY, uint16_t _width, uint16_t _height, char _hBorder, char _vBorder );
 };
 
 #endif
