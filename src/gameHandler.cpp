4b671219dd0b73c863890218d228c61dfbe150f6
zukonake <zeukmc@gmail.com>
2015-10-21 23:30:30 +0200

    Added gameHandler, it hold int main now.

---
+++ src/gameHandler.cpp
@@ -0,0 +1,18 @@
+//gameHandler.cpp
+#include "gameHandler.hpp"
+
+cGameHandler::cGameHandler( cGame _game ) :
+	game( _game )
+{
+
+}
+
+std::istream& operator>>( istream& os, cGameHandler& gameHandler )
+{
+	
+}
+
+std::ostream& operator<<( ostream& os, const cGameHandler& gameHandler )
+{
+	
+}
---
+++ src/gameHandler.hpp
@@ -0,0 +1,24 @@
+//gameHandler.hpp
+#ifndef GAMEHANDLER_HPP
+#define GAMEHANDLER_HPP
+
+#include <string>
+#include "game.hpp"
+
+class cGameHandler
+{
+	cGame* game;
+	cGameHandler();
+	friend std::istream& operator>>( istream& os, cGameHandler& gameHandler );
+	friend std::ostream& operator<<( ostream& os, const cGameHandler& gameHandler );
+}
+
+std::istream& operator>>( istream& os, cGameHandler& gameHandler );
+std::ostream& operator<<( ostream& os, const cGameHandler& gameHandler );
+
+int main( int argv, char argc[] )
+{
+	return 0;
+}
+
+#endif
