493edcd380daafe3cfdb4628e2166b1a00210703
zukonake <zeukmc@gmail.com>
2015-10-22 21:59:51 +0200

    Improved entity class structure.

--- src/entity/entity.hpp
+++ src/entity/entity.hpp
@@ -2,11 +2,24 @@
 #ifndef ENTITY_HPP
 #define ENTITY_HPP
 
+#include <string>
+
 #include "lua/luaTable.hpp"
+#include "entitySubtype.hpp"
+#include "entityControl.hpp"
 
 class cEntity
 {
+	cBoard* board;
+	cEntitySubtype* subtype;
+	cEntityControl* control;
 	cLuaTable componentList;
+	uint16_t posX;
+	uint16_t posY;
+	std::string name;
+	bool move( uint16_t targetPosX, uint16_t targetPosY );
+	cEntity( cLuaTable luaToCpp );
+	cEntity( std::string _name, cBoard _board, cLuaTable _componentList, uint16_t posX, uint16_t posY );
 }
 
 #endif
