58ff574417c29098ab5bf383a72cabeff2bb2abf
zukonake <zeukmc@gmail.com>
2015-11-22 16:20:49 +0100

    Added better lua examples.

---
+++ config/datasets/generic/entitySubtype/human.lua
@@ -0,0 +1,17 @@
+local human =
+{
+	tile =
+	{
+		base = "H",
+		foregroundColor = 05,
+		backgroundColor = 01
+	}
+	component =
+	{
+		name = "Human",
+		healthPoints = 100,
+		attackDamage = 10,
+		moveSpeed = 1.0,
+		fov = 10.0
+	}
+}
