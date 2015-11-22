c631cd0dfafa493f9f581659a8c4d05a07192b26
Seba123512 <seba12351212@gmail.com>
2015-11-22 18:49:25 +0100

    Added crate.lua

---
+++ config/datasets/Furnitures/entitySubtype/crate.lua
@@ -0,0 +1,17 @@
+local crate
+{
+	tile =
+	{
+		base = "#",
+		foregroundColor = 03,
+		backgroundColor = 01,
+		bold = false
+	}
+	component =
+	{
+		name = "Crate",
+		healthPoints = 20,
+		alive = false
+	}
+}
+   
