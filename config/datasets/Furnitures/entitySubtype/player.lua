cfebfa7aa054d87e0cd7d9f4c6e8a219d654ddc3
Seba123512 <seba12351212@gmail.com>
2015-11-23 19:08:21 +0100

    Added player.lua

---
+++ config/datasets/Furnitures/entitySubtype/player.lua
@@ -0,0 +1,20 @@
+locacl player =
+{
+  tille =
+  {
+    base = "$",
+    foregroundColor = 02,
+    backgroundColor = 00,
+    bold = true
+  }
+  component =
+	{
+		name = "player",
+		healthPoints = 100,
+		attackDamage = 0,
+		moveSpeed = 10,
+		fov = 10.0,
+		alive = true,
+		solid = false
+	}
+}
