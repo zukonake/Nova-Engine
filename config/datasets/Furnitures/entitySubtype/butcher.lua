dab3236a19f48bad4d5dd2bc469b1809756d76fb
Seba123512 <seba12351212@gmail.com>
2015-11-23 17:08:05 +0100

    Added butcher.lua

---
+++ config/datasets/Furnitures/entitySubtype/butcher.lua
@@ -0,0 +1,19 @@
+local butcher =
+{
+	tile =
+	{
+		base = "@",
+		foregroundColor = 01,
+		backgroundColor = 00,
+		bold = true
+	}
+	component =
+	{
+		name = "Butcher",
+		hp = 100,
+		ap = 50,
+		mp = 10,
+		alive = true,
+		solid = false
+	}
+}
