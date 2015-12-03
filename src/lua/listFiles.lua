b30a35b08c732b003df3b62af3f7f65787789ae8
zukonake <zeukmc@gmail.com>
2015-12-04 00:17:46 +0100

    Moved lua scripts to src/lua/script.

--- src/lua/listFiles.lua
+++
@@ -1,9 +0,0 @@
---listFiles.lua
-
-function listFiles( directoryPath )
-	local result = { }
-	for filename in io.popen( "ls " .. directoryPath ):lines() do
-		table.insert( result, filename )
-	end
-	return result
-end
