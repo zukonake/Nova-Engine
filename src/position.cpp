fbce64d05fc2f8397454428b06b9b9394ab81555
zukonake <zeukmc@gmail.com>
2015-12-09 23:06:53 +0100

    Introduced cPosition.

---
+++ src/position.cpp
@@ -0,0 +1,9 @@
+//position.cpp
+#include <position.hpp>
+
+cPosition::cPosition( uint _x, uint _y ) :
+	x( _x ),
+	y( _y )
+{
+
+}
---
+++ src/position.hpp
@@ -0,0 +1,14 @@
+//position.hpp
+#ifndef POSITION_HPP
+#define POSITION_HPP
+
+#include <typedef.hpp>
+
+struct cPosition
+{
+	uint x;
+	uint y;
+	cPosition( uint _x, uint _y );
+}
+
+#endif
