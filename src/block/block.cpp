6583ffaa109b5dadf828317422c68449d3770d38
zukonake <zeukmc@gmail.com>
2015-12-10 00:01:52 +0100

    Fixed syntax.

--- src/block/block.cpp
+++ src/block/block.cpp
@@ -7,13 +7,13 @@ void cBlock::render( cInterface* interface, uint posX, uint posY )
 	//TODO
 }
 
-cBlock( cBlockSubtype* _subtype ) :
+cBlock::cBlock( cBlockSubtype* _subtype ) :
 	subtype( _subtype ),
 {
 
 }
 
-cBlock( table luaToCpp, table* objectTable ) :
+cBlock::cBlock( table luaToCpp, table* objectTable ) :
 	subtype( boost::any_cast< blockSubtype* >( luaToCpp[ "subtype" ] ));
 {
 
